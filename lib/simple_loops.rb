a438946898468811b3642c78e9291e2aa562b089
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-29 01:43:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,6 @@
-counter = 0
-loop_message_five_times = ["Wolfie", "Erin", "Dino", "Nicole", "Iva"]
-while loop_message_five_times[counter] do
-  puts loop_message_five_times[counter]
-  counter += 1
-end
+def loop_message_five_times(message)
+  count = 0 
+  while count < 5 do
+    puts message
+    count +-1
+  end
